279b825d43fdf46ea009ce96054863df72c75f8a
Paul Cunningham <paul@locklan.com.au>
2014-12-03 15:50:21 +1000

    InstallAWSPowerShellModule.ps1 added

---
+++ InstallAWSPowerShellModule.ps1
@@ -0,0 +1,79 @@
+﻿<#
+.SYNOPSIS
+InstallAWSPowerShellModule.ps1
+
+.DESCRIPTION 
+Downloads and installs the Amazon Web Services Tools for Windows PowerShell. The script
+has been written as a function to make it easy to incorporate it into other scripts
+or your PowerShell profile.
+
+.OUTPUTS
+Script installs an MSI package downloaded from Amazon Web Services.
+
+.EXAMPLE
+.\InstallAWSPowerShellModule.ps1
+
+.LINK
+https://www.idea11.com.au/powershell-script-install-aws-powershell-tools
+
+.NOTES
+Written by: Paul Cunningham
+
+* Website:	http://www.idea11.com.au
+
+Find me on:
+
+* Twitter:	https://twitter.com/paulcunningham
+* LinkedIn:	http://au.linkedin.com/in/cunninghamp/
+* Github:	https://github.com/cunninghamp
+
+Change Log:
+V1.00, 3/12/2014 - Initial version
+#>
+
+Function InstallAWSToolsforWindowsPowerShell() {
+
+    $AWSPowerShellModuleSourceURL = "http://sdk-for-net.amazonwebservices.com/latest/AWSToolsAndSDKForNet.msi"
+    $DestinationFolder = "$ENV:homedrive\$env:homepath\Downloads"
+
+    If (!(Test-Path $DestinationFolder))
+    {
+        New-Item $DestinationFolder -ItemType Folder -Force
+    }
+
+    Write-Host "Downloading AWS PowerShell Module from $AWSPowerShellModuleSourceURL"
+
+    try
+    {
+        Invoke-WebRequest -Uri $AWSPowerShellModuleSourceURL -OutFile "$DestinationFolder\AWSToolsAndSDKForNet.msi" -ErrorAction STOP
+
+        ### MSI install reference: http://www.jonathanmedd.net/2012/07/automate-msi-installations-with-powershell.html
+        $msifile = "$DestinationFolder\AWSToolsAndSDKForNet.msi"
+
+        $arguments = @(
+                        "/i"
+                        "`"$msiFile`""
+                        "/qb"
+                        "/norestart"
+                        )
+
+        Write-Host "Attempting to install $msifile"
+
+        $process = Start-Process -FilePath msiexec.exe -ArgumentList $arguments -Wait -PassThru
+        if ($process.ExitCode -eq 0)
+        {
+            Write-Host "$msiFile has been successfully installed"
+        }
+        else
+        {
+            Write-Host "installer exit code  $($process.ExitCode) for file  $($msifile)"
+        }
+        ###
+    }
+    catch
+    {
+        Write-Host $_.Exception.Message
+    }
+}
+
+InstallAWSToolsforWindowsPowerShell
